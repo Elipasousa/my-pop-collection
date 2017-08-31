0c7e33b9afd98b629adebe723c602fdb3c7a110b
Elisabete Bicho <elisabete.bicho@forkit.pt>
2017-08-31 15:15:37 +0100

    Added Wishlist

---
+++ MyPopCollection/ViewControllers/WishlistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WishlistViewController.swift
+//  MyPopCollection
+//
+//  Created by Elisabete Sousa on 31/08/2017.
+//  Copyright © 2017 Elisabete Sousa. All rights reserved.
+//
+
+import UIKit
+
+class WishlistViewController: BaseViewViewController {
+
+    //MARK : - Outlets
+    
+    //MARK : - Vars
+    
+    //MARK : - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Setups
+    
+    override func setupViews() {
+        
+    }
+    
+    //MARK: - NavigationBar
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        self.title = "Wishlist"
+    }
+}
